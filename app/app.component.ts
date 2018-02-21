3f9c6b4cb27fea4ee4a45ad6569f46da6bb64d50
Guest User <Guest@Epicodus-3C.local>
2018-02-21 15:35:10 -0800

    seperate edit feature into its own component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,16 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({ //says "This Defines A New Component"
   selector: 'app-root',//corresponds to <app-root> in .html file
-  template: /*insert this where selector shows up*/`
+  /*insert template where selector shows up*/
+  template:`
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-    <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-    </ul>
+    <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"><task-list>
     <hr>
-    <div>
-      <h3>{{selectedTask.description}}</h3>
-      <p>Task Complete? {{selectedTask.done}}</p>
-    </div>
-    <div *ngIf="selectedTask">
-      <h3>Edit Task</h3>
-      <label>Enter Task Description:</label>
-      <input [(ngModel)]="selectedTask.description">
-      <label>Enter Task Priority (1-3):</label>
-      <br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-      <button (click)="finishedEditing()">Done</button>
-    </div>
+    <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
   </div>
   `
 })
@@ -37,7 +23,7 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
 
-  tasks: Task[] = [
+  masterTaskList: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
@@ -49,33 +35,7 @@ export class AppComponent {
     this.selectedTask = clickedTask;
   }
 
-  isDone(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("This task is not done. Better get to work!");
-    }
-  }
-
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-
   finishedEditing() {
     this.selectedTask = null;
-  }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public priority:number) { }
-  markDone(){
-    this.done=true;
   }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FormsModule }  from '@angular/forms';
+import { TaskListComponent }  from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
 
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ], //lists requirements for this module only
-  declarations: [ AppComponent ], //components this module controls
+  declarations: [ AppComponent,
+                  TaskListComponent,
+                  EditTaskComponent], //components this module controls
   bootstrap:    [ AppComponent ] //launch these components on load
 })
 
